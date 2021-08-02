048b3fc6c1334dd943ab948a8d6529e485d759f3
Dephrase <davidjf123@gmail.com>
2021-08-02 13:06:38 +0100

    Finished unit and integration tests

---
+++ cypress/integration/calculator.spec.js
@@ -0,0 +1,72 @@
+describe("Calculator", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  })
+
+  it('should have working number buttons', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2')
+  })
+
+  it('should update the running total when chaining operators', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('.display').should('contain', '4')
+  })
+
+  it('Should chain operations together', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '6')
+  })
+
+  it('Should return a negative number when minusing a larger number', () => {
+    cy.get('#number2').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-3')
+  })
+
+  it('Should return a large number', ()=>{
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3.086419749999691e+21')
+  })
+
+  it('Should return Inifinity when dividing by zero', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 'Infinity');
+  })
+})
